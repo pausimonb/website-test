b664a837fd3c8206af2bd01e34e95c3c6f5cbb84
pausimonb <130485335+pausimonb@users.noreply.github.com>
2023-05-04 12:08:21 -0600

    Add files via upload

---
+++ paginapadel.css
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="es_MX">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fundamentos para construir un Sitio Web</title>
+
+    <style type="text/css">
+    	body {
+    		background-color: whitesmoke;
+    		font-family: sans-serif;
+    	}
+    	
+    	h1 {
+    		color: green;
+    		font-size: 32px;
+    		font-style: italic;
+    		text-align: center;
+    	}
+
+    	h2 {
+    		color: darkgray;
+    		font-size: 28px;
+    		font-style: italic;
+    	}
+
+    	h3 {
+    		color: darkgray;
+    		font-size: 24px;
+    		font-style: italic;
+    	}
+
+    	p {
+    		text-align: justify-all;
+    		font-size: 24px;
+    	}
+
+    	.principal {
+    		background-color: white;
+    		padding: 50px;
+    		margin: 25px;
+    	}
+
+    	.codes {
+    		padding: 10px;
+    		margin: 10px;
+    		background-color: grey;
+    		font-size: 20px;
+    		color: darkred;
+    		text-align: center;
+    	}
+
+    	img {
+    		height: 500px;
+    		width: auto;
+    	}
+
+    </style>
+
+
+</head>
+
+<body>
+	<div class="principal">
+    <h1>El Padel: el mejor deporte del mundo</h1>
+    <p>
+        Un deporte que se ha popularizado con los años, hoy jugado en muchos lugares alrededor del mundo... ¡Veámos la razón detrás!
+    </p>
+    <h2>Historia</h2>
+    <p>
+        El pádel nació en Acapulco (México) en 1969 cuando Enrique Corcuera adaptó un terreno de su finca de 20 x 10 metros colocando paredes en los fondos y laterales, en principio para que la vegetación no invadiera su pista. Se jugaba con palas de madera y recibía el nombre de Paddle-Tenis.
+    </p>
+    <h2>La primera cancha de pádel en la hisotria</h2>
+    <p>
+        <img src="download.jpg" width="30px" height="30px">
+    </p>
+<h3>De México a España</h3>
+    <p>
+       Unos años más tarde, en 1974, Alfonso de Hohenlohe viaja a México invitado por su amigo Enrique. Durante su estancia se interesa por la práctica de ese deporte nuevo recién creado. Su entusiasmo por esta nueva actividad no se hizo esperar. A su vuelta a España, tras analizar y perfeccionar algunos detalles, construye las dos primeras pistas en Marbella Club. Así la historia del pádel fue dando sus primeros pasos.
+    </p>
+
+<h3>De México a Argentina</h3>
+    <p>
+       En 1975, un amigo argentino de Alfonso de Hohenlohe, decide importar el deporte a su país tras ser testigo directo del éxito que tiene. En Argentina es donde el pádel obtiene una importancia sin precedentes convirtiéndose en el segundo deporte más practicado.
+    </p>
+
+    <h2>Evolución</h2>
+    <ul>
+    	<li>1991: Se constituye en Madrid la Federación Internacional de Pádel. Se crea un reglamento a nivel mundial.</li>
+    	<li>1993: Se consolida como modalidad deportiva y se extiende a otros países de América como Brasil, Uruguay, Chile, etc.</li>
+    	<li>1997: Federación Española de Pádel.</li>
+        <li>2005: Nace el Pádel Pro Tour también conocido como PPT. Se crea un ranking mundial de pádel.</li>
+        <li>2013: World Padel Tour (WPT), el campeonato más importante del mundo del Pádel profesional.</li>
+    </ul>
+    
+
+    <h2>Rankings 2023</h2>
+
+    <table border="1px">
+    	<tr>
+    		<th>Femenino</th>
+    		<th>Masculino</th>
+    	</tr>
+    	<tr>
+    		<td>Paula Josemaría</td>
+    		<td>Adriana Sánchez</td>
+            <td>Alejandra Salazar</td>
+            <td>Gemma Triay</td>
+            <td>Beatriz González</td>
+    	</tr>
+    	<tr>
+    		<td>Juan Lebrón</td>
+    		<td>Alejandro Galán</td>
+            <td>Agustín Tapia</td>
+            <td>Arturo Coello</td>
+            <td>Carlos Daniel Gutiérrez</td>
+    	</tr>
+    	
+    </table>
+
+	</div>
+
+</body>
+
+</html>
